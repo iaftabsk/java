0c338e32672cbbf316ecb7ec9aff5d715a42ced8
Aftab <infotechaftab@gmail.com>
2021-10-22 01:35:51 +0530

    Sum of Two Number

---
+++ sumofnumber.java
@@ -0,0 +1,9 @@
+public class sumofnumber {
+    
+public static void main(String args[])   
+{  
+int n1 = 215, n2 = 100, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}
